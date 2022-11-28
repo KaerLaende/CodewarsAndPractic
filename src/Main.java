41e540023aca9b2bda3f394c159f2c54a1270c03
Kaerlaende <kaerlaende@mail.ru>
2022-11-28 14:05:36 +0300

    Поменяли вывод

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Привет, мир!");
     }
     //где же Changest в версии 2.38.1
 }
