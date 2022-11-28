010e1d81db143869cf715cb9dc0b807be154b9e7
Kaerlaende <kaerlaende@mail.ru>
2022-11-28 14:06:52 +0300

    Поменяли вывод

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, мир!");
     }
-    //где же Changest в версии 2.38.1
+    // версия 2.38.1 конец ноября 2022г.
 }
